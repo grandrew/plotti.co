5ef8ade009aa19a128e093fa9e28e13cab7202a8
Andrew Gryaznov <ag@vrayo.com>
2016-03-18 06:28:34 +0000

    valuecache wip

--- evsvg.py
+++ evsvg.py
@@ -1,4 +1,4 @@
-import flask, time, random, optparse
+import flask, time, random, optparse, collections, string, re
 from flask import request
 from flask.ext.cache import Cache
 import gevent
@@ -6,6 +6,8 @@ from gevent.wsgi import WSGIServer
 from gevent.queue import Queue
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+
+VALUE_CACHE_MAXAGE = 600
 
 app = flask.Flask(__name__)
 cache = Cache(app,config={'CACHE_TYPE': 'simple'})
@@ -17,7 +19,84 @@ limiter = Limiter(
 
 subscriptions = {}
 lhosts = {}
+value_cache = collections.OrderedDict()
 
+class ExpiringDeque(collections.deque):
+    def __init__(self, data):
+        super(ExpiringDeque, self).__init__(maxlen=50)
+        self.append(data)
+        self.ts = time.time()
+    def is_expired(self):
+        if time.time() - self.ts > VALUE_CACHE_MAXAGE:
+            return True
+        return False
+    def update(self):
+        self.ts = time.time()
+
+def value_cache_clean_one():
+    d = value_cache.popitem(False)
+    if not d[1].is_expired():
+        value_cache[d[0]]=d[1]
+
+allc=string.maketrans('','')
+nodigs=allc.translate(allc, string.digits+".")
+def parseFloat(txt):
+    try:
+        return float(txt)
+    except ValueError:
+        try:
+            return float(txt.translate(allc, nodigs))
+        except ValueError:
+            return None
+rmsg = re.compile("[\\d.,]+")
+SUPS=["","k","M","G","T","e"]
+SUPS_LEN = len(SUPS)
+def generate_points(dlist):
+    msg=""
+    data=[]
+    max_val=0
+    neg_val=False
+    for d in dlist:
+        vals = [parseFloat(x) for x in d[0].split(",")]
+        m=max(vals)
+        if m > max_val:
+            max_val = m
+        if min(vals) < 0:
+            neg_val = True
+        data.append(vals)
+        msg = rmsg.sub("", d[0])
+    timeMid = (dlist[0][1] - dlist[-1][1]) / 2
+    max_val = axis_max(max_val)
+    mid_val = max_val / 2
+    
+    mid_idx = 0
+    while mid_val >= 1e3 and mid_idx < SUPS_LEN-1:
+        mid_val /= 1e3
+        mid_idx += 1
+    if(mid_val > 1) mid_val = round(mid_val, 2);
+    if(mid_val > 0) mid_val = round(mid_val, 2); # TODO: think here and in SVG, 3-d precision is when v < 0.3 https://github.com/grandrew/plotti.co/issues/11
+    valueMid = "%s%s" % (mid_val, SUPS[mid_idx])
+    
+    points = ""
+    oldx = 0
+    for vals in data:
+        onerow="<g>"
+        r = 0
+        oldy = 0 # TODO fix 0-problem https://github.com/grandrew/plotti.co/issues/10
+        x = oldx + 500 / 50 # width / pts
+        for y in vals:
+            if r == 0:
+                oldy = y
+            else:
+                onerow += '<polyline class="src%s" points="%s,%s %s,%s"/>' % (r, oldx,oldy,x,y)
+            oldy = y
+            r+=1
+        points+=onerow
+        oldx = x
+    
+    return max_val, valueMid, timeMid, neg_val, points+"</g>" # trdn=20
+        
+    
 @cache.cached(timeout=500)
 @app.route( '/css/main.css' )
 def main_css():
@@ -36,7 +115,10 @@ def plot(hashstr):
 @cache.cached(timeout=500)
 @app.route( '/<hashstr>/<width>x<height>.svg' )
 def plotwh(hashstr,width,height):
+    value_cache_clean_one()
     svg = file('main.svg','r').read()
+    if hashstr in value_cache:
+        
     if width and height: svg = svg.replace('height="210" width="610"', 'height="%s" width="%s"' % (height, width))
     return flask.Response(svg,  mimetype= 'image/svg+xml')
 
@@ -56,13 +138,21 @@ def lock(hashstr):
 @app.route('/<hashstr>', methods=['GET'])
 def feeder(hashstr):
     data = request.args.get('d')
+    if len(data) > 1024:
+        return ""
     if not data:
         return plot(hashstr)
+    if not hashstr in value_cache:
+        value_cache[hashstr] = ExpiringDeque(data);
+    else:
+        value_cache[hashstr].append((data,int(time.time())))
+        value_cache[hashstr].update()
+        
     if not hashstr in subscriptions: return ""
     if request.headers.getlist("X-Forwarded-For"):
-       ip = request.headers.getlist("X-Forwarded-For")[0]
+        ip = request.headers.getlist("X-Forwarded-For")[0]
     else:
-       ip = request.remote_addr
+        ip = request.remote_addr
     try:
         if hashstr in lhosts and ip != lhosts[hashstr]: return ""
     except KeyError:
